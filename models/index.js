ca4e8b1cf960c90105db35e7eeee672844180856
Horacio Gutierrez <hogutier@yahoo.com>
2018-06-01 10:09:56 -0500

    complete Querying with Models

--- models/index.js
+++ models/index.js
@@ -19,6 +19,18 @@ const Page = db.define('page', {
   }
 });
 
+function generateSlug (title) {
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
+
+Page.beforeValidate((pageInstance) => {
+  if (!pageInstance.slug){
+    pageInstance.slug = generateSlug(pageInstance.title)
+  }
+})
+
 const User = db.define('user', {
   name: {
     type: Sequelize.STRING,
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,23 @@
 
 const router = require('express').Router()
+const { Page } = require('../models')
 const { addPage, editPage, main, wikiPage } = require('../views')
 
 router.get('/', (req, res, next) => {
   res.send('got to GET /wiki/')
 })
 
-router.post('/', (req, res, next) => {
-  res.json(req.body)
+router.post('/', async(req, res, next) => {
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  })
+  try {
+    await page.save();
+    res.redirect('/')
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/add', (req, res, next) => {
